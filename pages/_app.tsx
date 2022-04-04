435c5bf02c2919b78cc152a5336c4363f3b53533
hjdisley <hjdisley@gmail.com>
2022-04-04 21:33:43 +0100

    Completed basic routing

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,7 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
+export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
 
-// Initial commit
-
-export default MyApp
---
+++ pages/portfolio/[id].tsx
@@ -0,0 +1,7 @@
+export default function PortfolioProject() {
+  return (
+    <div>
+      <h1>Portfolio Project Page</h1>
+    </div>
+  );
+}
